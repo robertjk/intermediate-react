472519ad97cd740d7c2094b853d7c6d185d6962a
Robert J. Kusznier <no-cheating@autistici.org>
2025-08-07 15:26:11 +0200

    Update dirname in ssg

--- projects/static-site-generation/build.js
+++ projects/static-site-generation/build.js
@@ -1,19 +1,16 @@
 import { renderToStaticMarkup } from "react-dom/server";
 import { createElement } from "react";
 import { readFile, writeFile, mkdir, readdir, unlink } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import { join } from "node:path";
 
 import App from "./App.js";
 
-function dirnameForESModules() {
-  const __filename = fileURLToPath(import.meta.url);
-  return dirname(__filename);
-}
-
-async function createHTML(__dirname, App) {
+async function createHTML(App) {
   const app = renderToStaticMarkup(createElement(App));
-  const htmlSrc = await readFile(join(__dirname, "index.html"), "utf-8");
+  const htmlSrc = await readFile(
+    join(import.meta.dirname, "index.html"),
+    "utf-8"
+  );
   return htmlSrc.replace("{{ app }}", app);
 }
 
@@ -29,9 +26,8 @@ async function writeHTML(path, filename, html) {
   await writeFile(join(path, filename), html);
 }
 
-const __dirname = dirnameForESModules();
-const html = await createHTML(__dirname, App);
-const distPath = join(__dirname, "dist");
+const html = await createHTML(App);
+const distPath = join(import.meta.dirname, "dist");
 await removeExistingFiles(distPath);
 await writeHTML(distPath, "index.html", html);
 
