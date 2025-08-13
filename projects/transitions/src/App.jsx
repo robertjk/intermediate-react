40f365f1adc1b263641751ba13c1516910a84702
Robert J. Kusznier <no-cheating@autistici.org>
2025-08-13 21:27:48 +0200

    Implement showing game score in App

--- projects/transitions/src/App.jsx
+++ projects/transitions/src/App.jsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+
+import Score from "./Score";
+import getScore from "./getScore";
+
 export default function App() {
-  return <h1>Transitions</h1>;
+  const [isPending, setIsPending] = useState(true);
+  const [game, setGame] = useState(1);
+  const [score, setScore] = useState({ home: "-", away: "-" });
+
+  async function getNewScore(game) {
+    setIsPending(true);
+    setGame(game);
+    const newScore = await getScore(game);
+    setScore(newScore);
+    setIsPending(false);
+  }
+
+  function handleSelectChange(event) {
+    setGame(event.target.value);
+  }
+
+  useEffect(() => {
+    getNewScore(game);
+  }, [game]);
+
+  return (
+    <div className="app">
+      <h1>Game {game}</h1>
+      <select disabled={isPending} onChange={handleSelectChange}>
+        {[1, 2, 3, 4, 5, 6, 7].map((gameId) => (
+          <option key={gameId} value={gameId}>
+            Game {gameId}
+          </option>
+        ))}
+      </select>
+      <div className={`loading-container ${isPending ? "loading" : ""}`}>
+        {" "}
+        <span className="spinner">🌀</span>
+      </div>
+      <Score
+        isPending={isPending}
+        home={score.home}
+        homeImage={score.homeImage}
+        homeName={score.homeName}
+        away={score.away}
+        awayImage={score.awayImage}
+        awayName={score.awayName}
+      />
+    </div>
+  );
 }
